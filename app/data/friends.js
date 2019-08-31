1160464eb1d67f721ae09f8d59c12031901551f1
Katherine Walters <kattymay222@gmail.com>
2019-08-31 11:51:03 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+      "name": "Malcolm",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/3/39/Mal.jpg/revision/latest?cb=20080301061839",
+      "scores": [1, 3, 4, 4, 5, 5, 3, 5, 5, 4]
+  },
+  {
+      "name": "Zoe",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/1/10/Zoe.jpg/revision/latest?cb=20080301064448",
+      "scores": [1, 1, 2, 4, 3, 3, 3, 5, 4, 1]
+  },
+  {
+      "name": "Wash",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/f/fa/Wash.jpg/revision/latest?cb=20080301062346",
+      "scores": [1, 4, 2, 4, 2, 2, 4, 5, 2, 3]
+  },
+  {
+      "name": "Kaylee",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/5/55/Kaylee.jpg/revision/latest?cb=20100426173058",
+      "scores": [1, 1, 1, 2, 2, 2, 2, 4, 3, 2]
+  },
+  {
+      "name": "Shepherd Book",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/e/e2/Book.jpg/revision/latest?cb=20080301063245",
+      "scores": [2, 1, 1, 2, 2, 1, 1, 4, 2, 3]
+  },
+  {
+      "name": "Inara",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/c/c3/Inara02.jpg/revision/latest?cb=20080301063009",
+      "scores": [1, 1, 1, 4, 3, 2, 5, 4, 3, 1]
+  },
+  {
+      "name": "Simon",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/6/63/Simon2.jpg/revision/latest?cb=20080308044029",
+      "scores": [1, 1, 5, 5, 2, 4, 3, 5, 3, 1]
+  },
+  {
+      "name": "River",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/2/2c/River.jpg/revision/latest?cb=20080301023737",
+      "scores": [1, 5, 5, 5, 2, 4, 3, 5, 4, 1]
+  },
+  {
+      "name": "Jayne",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/d/d0/Jayne.jpg/revision/latest?cb=20080301065044",
+      "scores": [1, 5, 5, 1, 5, 5, 1, 5, 5, 1]
+  },
+  {
+      "name": "YoSaffBridge",
+      "photo": "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2015%2F01%2Ffirefly-christina-hendricks_320.jpg%3Fw%3D320&w=700&q=85",
+      "scores": [1, 1, 5, 5, 5, 5, 4, 5, 5, 1]
+  },
+  {
+      "name": "Patience",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/1/16/Patience.jpg/revision/latest?cb=20080317010321",
+      "scores": [1, 1, 3, 3, 4, 5, 2, 5, 5, 1]
+  },
+  {
+      "name": "Niska",
+      "photo": "https://vignette.wikia.nocookie.net/firefly/images/6/69/Niska.jpg/revision/latest?cb=20080307031032",
+      "scores": [5, 1, 1, 3, 5, 5, 1, 1, 3, 1]   
+  }
+];
---
+++ routing/apiRoutes.js
@@ -0,0 +1,65 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+//===============================================================================
+
+var friendList = require("../data/friend.js");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendList);
+    });
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+
+    app.post('/api/new', function (req, res) {
+
+        var scoreDifference;
+        var friendScore;
+        var userScore;
+        var newUser = req.body;
+
+        var bestMatch = {
+            name: "",
+            friendDiff: Infinity
+        };
+
+        for (var i = 0; i < friends.length; i++) {
+            var currentFriend = friends[i];
+            scoreDifference = 0;
+
+            for (var j = 0; j < newUser.scores.length; j++) {
+                friendScore = currentFriend.scores[j];
+
+                userScore = newUser.scores[j];
+                scoreDifference += Math.abs(parseInt(userScore) - parseInt(friendScore));
+            }
+
+            if (scoreDifference <= bestMatch.friendDiff) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDiff = scoreDifference;
+            }
+
+
+        }
+        res.json(bestMatch);
+    });
+};
