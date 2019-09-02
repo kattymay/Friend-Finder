aa3cffa71f753edc42c3be28a7df93cff2841fd2
Katherine Walters <kattymay222@gmail.com>
2019-09-02 12:14:53 -0400

    updated friendsList

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,62 +1,47 @@
 module.exports = [
   {
-      "name": "Malcolm",
+      "name": "Jessie",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/3/39/Mal.jpg/revision/latest?cb=20080301061839",
       "scores": [1, 3, 4, 4, 5, 5, 3, 5, 5, 4]
   },
   {
-      "name": "Zoe",
+      "name": "Keriann",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/1/10/Zoe.jpg/revision/latest?cb=20080301064448",
       "scores": [1, 1, 2, 4, 3, 3, 3, 5, 4, 1]
   },
   {
-      "name": "Wash",
+      "name": "QC",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/f/fa/Wash.jpg/revision/latest?cb=20080301062346",
       "scores": [1, 4, 2, 4, 2, 2, 4, 5, 2, 3]
   },
   {
-      "name": "Kaylee",
+      "name": "Evie",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/5/55/Kaylee.jpg/revision/latest?cb=20100426173058",
       "scores": [1, 1, 1, 2, 2, 2, 2, 4, 3, 2]
   },
   {
-      "name": "Shepherd Book",
+      "name": "Conner",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/e/e2/Book.jpg/revision/latest?cb=20080301063245",
       "scores": [2, 1, 1, 2, 2, 1, 1, 4, 2, 3]
   },
   {
-      "name": "Inara",
+      "name": "Kuti",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/c/c3/Inara02.jpg/revision/latest?cb=20080301063009",
       "scores": [1, 1, 1, 4, 3, 2, 5, 4, 3, 1]
   },
   {
-      "name": "Simon",
+      "name": "Stephen",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/6/63/Simon2.jpg/revision/latest?cb=20080308044029",
       "scores": [1, 1, 5, 5, 2, 4, 3, 5, 3, 1]
   },
   {
-      "name": "River",
+      "name": "James",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/2/2c/River.jpg/revision/latest?cb=20080301023737",
       "scores": [1, 5, 5, 5, 2, 4, 3, 5, 4, 1]
   },
   {
-      "name": "Jayne",
+      "name": "Marcy",
       "photo": "https://vignette.wikia.nocookie.net/firefly/images/d/d0/Jayne.jpg/revision/latest?cb=20080301065044",
       "scores": [1, 5, 5, 1, 5, 5, 1, 5, 5, 1]
   },
-  {
-      "name": "YoSaffBridge",
-      "photo": "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2015%2F01%2Ffirefly-christina-hendricks_320.jpg%3Fw%3D320&w=700&q=85",
-      "scores": [1, 1, 5, 5, 5, 5, 4, 5, 5, 1]
-  },
-  {
-      "name": "Patience",
-      "photo": "https://vignette.wikia.nocookie.net/firefly/images/1/16/Patience.jpg/revision/latest?cb=20080317010321",
-      "scores": [1, 1, 3, 3, 4, 5, 2, 5, 5, 1]
-  },
-  {
-      "name": "Niska",
-      "photo": "https://vignette.wikia.nocookie.net/firefly/images/6/69/Niska.jpg/revision/latest?cb=20080307031032",
-      "scores": [5, 1, 1, 3, 5, 5, 1, 1, 3, 1]   
-  }
 ];
